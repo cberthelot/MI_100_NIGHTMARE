147e9ef65765fe41165f389261d5f4af53daa5e0
macgyver3 <101329236+macgyver3@users.noreply.github.com>
2022-03-10 12:05:29 +0100

    Add files via upload

---
+++ NIGHTMARE.X/build/default/production/chenillard.o.d
@@ -0,0 +1,4 @@
+build/default/production/chenillard.o:  \
+chenillard.c  \
+config_bits.h  \
+ minimal_lcd.h 
---
+++ NIGHTMARE.X/build/default/production/minimal_lcd.o.d
@@ -0,0 +1,3 @@
+build/default/production/minimal_lcd.o:  \
+minimal_lcd.c  \
+minimal_lcd.h 
