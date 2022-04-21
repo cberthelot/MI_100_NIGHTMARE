0beabd7d02fb38ba8ea312edd5bb06ec977931ed
Pierre Cazenave <47040310+beexto@users.noreply.github.com>
2022-04-21 15:24:52 +0200

    final clean

--- NIGHTMARE.X/build/default/production/chenillard.o.d
+++
@@ -1,5 +0,0 @@
-build/default/production/chenillard.o:  \
-chenillard.c  \
-config_bits.h  \
- library.h  \
-minimal_lcd.h 
---
+++ NIGHTMARE.X/periph.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   periph.h
+ * Author: pierr
+ *
+ * Created on 21 avril 2022, 15:22
+ */
+
+#ifndef PERIPH_H
+#define	PERIPH_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* PERIPH_H */
+
